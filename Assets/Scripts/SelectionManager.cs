365c30afc6de1fd84409070edd6be07e295a2c74
Matthew Au Yeung <galaxys2.matthew0621@gmail.com>
2021-04-27 17:25:16 -0700

    [Matthew] - Updated the Selection scene. - Added choice for player to choose between randomize or manually choosing the enemy. - Added dialog box in the selection scene.

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -6,15 +6,82 @@ using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
+    public enum SelectionState { None, PlayerPokemonSelection, ChooseEnemySelection, ManualEnemySelection, RandomEnemySelecion }
+
     public enum PokemonSelection { charizard, decidueye, dragpult, electivire, empoleon, garchomp, gardevoir, greninja, hydreigon, jirachi, lapras, lucario, lycanroc, metagross, salamence, snorlax, sylveon, toxicroak, vikavolt }
     public List<Icon> selectionIcons;
 
+    [SerializeField]
+    private SelectionDialogBox dialogBox;
+
+    private SelectionState selectionState = SelectionState.None;
     private int currentSelectIndex;
     private int enemySelectionIndex;
     private float timer = 0.0f;
     private bool startRandomize = false;
 
+    private void Start()
+    {
+        StartCoroutine(dialogBox.TypeDialog("Select your Pokemon..."));
+        selectionState = SelectionState.PlayerPokemonSelection;
+    }
+
     private void Update()
+    {
+        UpdateIconSelection(currentSelectIndex);
+
+        switch (selectionState)
+        {
+            case SelectionState.None:
+                break;
+            case SelectionState.PlayerPokemonSelection:
+                PlayerPokemonSelection();
+                break;
+            case SelectionState.ChooseEnemySelection:
+                ChooseEnemySeleciton();
+                break;
+            case SelectionState.ManualEnemySelection:
+                ManaulEnemySelection();
+                break;
+            case SelectionState.RandomEnemySelecion:
+                RandomEnemySelection();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void PlayerPokemonSelection()
+    {
+        SelectionInput(true);
+    }
+
+    private void ChooseEnemySeleciton()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(dialogBox.TypeDialog("Randomizing enemy Pokemon..."));
+            selectionState = SelectionState.RandomEnemySelecion;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            StartCoroutine(dialogBox.TypeDialog("Select enemy Pokemon..."));
+            selectionState = SelectionState.ManualEnemySelection;
+        }
+    }
+
+    private void ManaulEnemySelection()
+    {
+        SelectionInput(false);
+    }
+
+    private void RandomEnemySelection()
+    {
+        timer += Time.deltaTime;
+        RandomSelection();
+    }
+
+    private void SelectionInput(bool isPlayer)
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
@@ -41,24 +108,24 @@ public class SelectionManager : MonoBehaviour
         {
             AudioManager.instance.PlaySound("Button");
 
-            if (currentSelectIndex > 1)
+            if (currentSelectIndex > 3)
                 currentSelectIndex -= 4;
         }
-
-        UpdateIconSelection(currentSelectIndex);
 
         if (Input.GetKeyDown(KeyCode.Z))
         {
             AudioManager.instance.PlaySound("Button");
-            Player.instance.pokemonBase = selectionIcons[currentSelectIndex].pokemonBase;
-
-            startRandomize = true;
-        }
-
-        if(startRandomize)
-        {
-            timer += Time.deltaTime;
-            RandomSelection();
+            if (isPlayer)
+            {
+                Player.instance.pokemonBase = selectionIcons[currentSelectIndex].pokemonBase;
+                StartCoroutine(dialogBox.TypeDialog("Press 'R' to randomize or 'M' to choose your enemy..."));
+                selectionState = SelectionState.ChooseEnemySelection;
+            }
+            else
+            {
+                Enemy.instance.pokemonBase = selectionIcons[currentSelectIndex].pokemonBase;
+                ToBattleScene();
+            }
         }
     }
 
@@ -82,9 +149,13 @@ public class SelectionManager : MonoBehaviour
         else if(timer > 4.0f)
         {
             Enemy.instance.pokemonBase = selectionIcons[currentSelectIndex].pokemonBase;
-            AudioManager.instance.StopSound("MainTheme");
-            AudioManager.instance.PlaySound("BattleTheme");
-            SceneManager.LoadScene("BattleScene");
+            ToBattleScene();
         }
+    }
+    private void ToBattleScene()
+    {
+        AudioManager.instance.StopSound("MainTheme");
+        AudioManager.instance.PlaySound("BattleTheme");
+        SceneManager.LoadScene("BattleScene");
     }
 }
